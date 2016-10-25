f244ee77aa9e410f06631a81d18767acb2eaa06f
shimanyi <vshimanyi@gmail.com>
2016-10-25 23:51:38 +0300

    slight model changes

--- oosc/oosc/constituencies/models.py
+++ oosc/oosc/constituencies/models.py
@@ -4,13 +4,8 @@ from oosc.counties.models import Counties
 # Create your models here.
 
 class Constituencies(models.Model):
-<<<<<<< HEAD
     constituency = models.CharField(max_length=200)
     county_id = models.ForeignKey(Counties, on_delete=models.CASCADE)
 
     def __str__(self):
         return ('constituency')
-=======
-    cons_name = models.CharField(max_length=200)
-    county_id = models.ForeignKey(Counties, on_delete=models.CASCADE)
->>>>>>> f6fa739de7087389f448ff1647ab1cd8a3023aa1
--- oosc/oosc/schools/models.py
+++ oosc/oosc/schools/models.py
@@ -4,7 +4,6 @@ from oosc.constituencies.models import Constituencies
 # Create your models here.
 class Schools(models.Model):
     school_code = models.IntegerField(default=0)
-<<<<<<< HEAD
     school_name = models.CharField(max_length = 200, default="schoolname")
     geo_cordinates  = models.CharField(max_length = 200)
     emis_code   = models.IntegerField(default = 0)
@@ -15,12 +14,3 @@ class Schools(models.Model):
 
     def __str__(self):
         return ('school_name')
-=======
-    school_name = models.CharField(max_length = 200)
-    geo_cordinates  = models.CharField(max_length = 200)
-    emis_code   = models.IntegerField(default = 0)
-    cons_id     = models.ForeignKey(Constituencies, on_delete = models.CASCADE)
-    source_of_water = models.CharField(max_length = 200)
-    headteacher = models.CharField(max_length=200)
-    phone_no    = models.IntegerField(default=0)
->>>>>>> f6fa739de7087389f448ff1647ab1cd8a3023aa1
