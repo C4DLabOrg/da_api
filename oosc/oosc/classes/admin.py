ff383d452380000097f4b783ccead58658329732
Mwangi Micha <michameiu@gmail.com>
2016-10-28 21:13:15 +0300

    added the attendance end point

--- oosc/oosc/classes/admin.py
+++ oosc/oosc/classes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from oosc.classes.models import Classes
 # Register your models here.
+
+class classAdmin(admin.ModelAdmin):
+    list_display=['class_name','school_id','teacher_id']
+
+admin.site.register(Classes,classAdmin);
