df2b5d5fb861f1fa2ff3997e3188fe0b78f05dbe
mwangi micha <michameiu@gmail.com>
2017-01-09 10:59:21 +0300

    switched to database development mode

--- oosc/oosc/schools/views.py
+++ oosc/oosc/schools/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from oosc.schools.models import Schools
 from rest_framework import generics
 from oosc.schools.serializers import SchoolsSerializer
+from rest_framework.permissions import IsAdminUser
 # Create your views here.
 
 class ListCreateSchool(generics.ListCreateAPIView):
     queryset=Schools.objects.all();
     serializer_class=SchoolsSerializer
+    permission_classes = (IsAdminUser,)
