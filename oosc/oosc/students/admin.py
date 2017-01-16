f80d90fb3d04918eb7edcb7cd4d8d5b46c3584c3
mwangi micha <michameiu@gmail.com>
2017-01-16 17:22:42 +0300

    fiexed

--- oosc/oosc/students/admin.py
+++ oosc/oosc/students/admin.py
@@ -3,6 +3,6 @@ from oosc.students.models import Students
 # Register your models here.
 
 class classAdmin(admin.ModelAdmin):
-    list_display=['id','emis_code','student_name','absence','date_of_birth','admission_no','class_id','gender','previous_class','mode_of_transport','time_to_school','stay_with','household','meals_per_day','not_in_school_before','emis_code_histories','total_attendance']
+    list_display=['id','emis_code','student_name','total_absents','date_of_birth','admission_no','class_id','gender','previous_class','mode_of_transport','time_to_school','stay_with','household','meals_per_day','not_in_school_before','emis_code_histories','total_attendance']
 
 admin.site.register(Students,classAdmin)
