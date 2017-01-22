d6eb5ab56bdc9e921c0cb20dad07fb90430f94fa
mwangi micha <michameiu@gmail.com>
2017-01-22 16:52:58 +0300

    added date enrolled

--- oosc/oosc/students/serializers.py
+++ oosc/oosc/students/serializers.py
@@ -5,7 +5,7 @@ class StudentsSerializer(serializers.ModelSerializer):
     student_name=serializers.SerializerMethodField()
     class Meta:
         model = Students
-        fields = ('student_id', 'emis_code','last_attendance','total_absents','student_name', 'fstname','midname','lstname','date_of_birth', 'admission_no', 'class_id',
+        fields = ('student_id','date_enrolled', 'emis_code','last_attendance','total_absents','student_name', 'fstname','midname','lstname','date_of_birth', 'admission_no', 'class_id',
         'gender', 'previous_class', 'mode_of_transport','time_to_school', 'stay_with', 'household',
         'meals_per_day', 'not_in_school_before', 'emis_code_histories')
     def get_student_name(self,obj):
@@ -16,7 +16,7 @@ class SimpleStudentSerializer(serializers.ModelSerializer):
     student_name = serializers.SerializerMethodField()
     class Meta:
         model=Students
-        fields=('id','student_id','student_name', 'emis_code','last_attendance','total_absents', 'fstname','midname','lstname')
+        fields=('id','student_id','student_name','date_enrolled', 'emis_code','last_attendance','total_absents', 'fstname','midname','lstname')
 
     def get_student_name(self, obj):
         return obj.fstname + " " + obj.lstname
