8e395a45a159d22c8034af97f263c6bffc9f9e2f
Micha Mwangi <michameiu@gmail.com>
2017-02-19 23:08:00 +0300

    added the students import link

--- oosc/oosc/students/models.py
+++ oosc/oosc/students/models.py
@@ -28,5 +28,6 @@ class Students(models.Model):
     total_absents=models.IntegerField(default=0,null=True,blank=True)
     last_attendance=models.DateField(null=True,blank=True)
 
+
     def __str__(self):
         return self.lstname
--- oosc/oosc/students/views.py
+++ oosc/oosc/students/views.py
@@ -5,6 +5,13 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from oosc.students.serializers import StudentsSerializer
 from datetime import datetime,timedelta
+import csv
+from rest_framework import serializers
+import json
+from oosc.classes.models import Classes
+from oosc.schools.models import Schools
+from oosc.teachers.models import Teachers
+from datetime import datetime
 # Create your views here.
 
 class ListCreateStudent(generics.ListCreateAPIView):
@@ -21,3 +28,111 @@ class GetEnrolled(APIView):
         males=studs.filter(gender='M')
         return Response({"total":len(studs),"males":len(males),
                          "females":len(females),"students":ser.data},status=status.HTTP_200_OK)
+
+class ImportStudentSerializer(serializers.Serializer):
+    fstname=serializers.CharField(max_length=50)
+    midname=serializers.CharField(max_length=50,required=False,allow_null=True,allow_blank=True)
+    lstname=serializers.CharField(max_length=50,required=False,allow_null=True,allow_blank=True)
+    school=serializers.IntegerField()
+    clas=serializers.CharField(max_length=50)
+    gender=serializers.CharField(max_length=20)
+    #date_enrolled=serializers.DateField(required=False,allow_null=True)
+    # emis_code=serializers.IntegerField(required=False,allow_null=True)
+
+
+def set_class(s):
+    s=s.split(' ')[1]
+    if s=="ECD":
+        return "Std 1"
+    return "Std "+str(int(s)+1)
+
+def get_gender(s):
+    if s.lower() =="male":
+        return "M"
+    elif s.lower()=='female':
+        return "F"
+def valid_date(date_text):
+    try:
+        datetime.strptime(date_text,'%Y-%m-%d')
+        return True
+    except ValueError:
+        return False
+
+class ImportStudents(APIView):
+    def post(self,request,format=None):
+        file=request.FILES["file"]
+        data = [row for row in csv.reader(file.read().splitlines())]
+        d=""
+        s=0
+        rowindex=[]
+        err=""
+        the_data=data[1:]
+        for i,dat in enumerate(the_data):
+            dt={"fstname":dat[6],"midname":dat[7],"lstname":dat[8], "school":dat[5],
+                "clas":dat[13],"gender":dat[11]}
+            ser=ImportStudentSerializer(data=dt)
+
+            if ser.is_valid():
+                sch=Schools.objects.filter(emis_code=ser.data.get("school"))
+                teach=Teachers.objects.all()
+                if(sch.exists()):
+                    sch=sch[0]
+                    if(not teach.exists()):
+                        return Response("Create atleast one Teacher for the school")
+                    else:
+                        teach=teach[0]
+                else:
+                    print (ser.data.get("school"))
+                    return Response("Create School First")
+                nxt_class=set_class(ser.data.get("clas"))
+                if not nxt_class == "Std 9":
+                    cls=Classes.objects.filter(class_name=nxt_class)
+                    cl = Classes()
+                    if(cls.exists()):
+                        cl=cls[0]
+                    else:
+                        cl.class_name=nxt_class
+                        cl.school=sch
+                        cl.teacher=teach
+                        cl.save()
+                    std=Students.objects.filter(fstname=ser.data.get("fstname"),lstname=ser.data.get("lstname"),midname=ser.data.get("midname"),
+                                                class_id=cl)
+                    #check if student Exists
+                    if(std.exists()):
+                        #print "Found"
+                        pass
+                    else:
+                        std=Students()
+                        std.fstname=ser.data.get("fstname")
+                        std.midname=ser.data.get("midname")
+                        std.lstname=ser.data.get("lstname")
+                        std.gender=get_gender(ser.data.get("gender"))
+                        std.class_id=cl
+                        if(valid_date(dat[2])):
+                            std.date_enrolled=dat[2]
+                        else:
+                            std.date_enrolled=datetime.now()
+                        std.save()
+                        s += 1
+
+                else:
+                    print("Done Kcpe")
+            else:
+                err=ser.errors
+            # schl=dat[5]
+            # clas=dat[6]
+            # if  clas and schl:
+            #     pass
+            # else:
+            #     print dat
+            #     return Response("Make Sure The Data is correct "+str(i)+" "+dat[6])
+            # s=i
+            # d=dat[4]
+        f=s
+        s=int((float(s)/float(len(the_data)))*100)
+        rowindex.append({"name":"Import Summary","index":str(s)+"%, "+str(f)+" of  "+str(len(the_data))})
+        rowindex.append({"name":"Errors","index":err})
+        for i,dat in enumerate(data[0]):
+            rw={'name':dat,'index':i}
+            rowindex.append(rw)
+        return Response(data=json.loads(json.dumps(rowindex)))
