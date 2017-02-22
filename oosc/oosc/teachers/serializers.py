955298e6ba227f5ec4b6396d31566926ee5eec23
Micha Mwangi <michameiu@gmail.com>
2017-02-22 16:12:28 +0300

    fetch classes ordered by class name

--- oosc/oosc/teachers/serializers.py
+++ oosc/oosc/teachers/serializers.py
@@ -39,7 +39,7 @@ class TeacherAllSerializer(serializers.ModelSerializer):
         return ser.data
 
     def get_classes(self,obj):
-        queryset=Classes.objects.filter(teacher =obj.id)
+        queryset=Classes.objects.filter(teacher =obj.id).order_by("class_name",)
         ser=StudentsClassSerializer(queryset,many=True)
         return ser.data
     def get_profile(self,obj):
