07a83cff5adca11f89fe50d97e01d99e21ddc984
Micha Mwangi <michameiu@gmail.com>
2017-03-11 19:42:13 +0300

    added the bulk update option for faster update

--- oosc/oosc/attendance/models.py
+++ oosc/oosc/attendance/models.py
@@ -9,7 +9,7 @@ class Attendance(models.Model):
     student  = models.ForeignKey(Students, on_delete = models.CASCADE)
     date    = models.DateTimeField()
     created=models.DateTimeField(default=datetime.now)
-    modified=models.DateTimeField(auto_now=True)
+    modified=models.DateTimeField(default=datetime.now)
     status  = models.IntegerField(choices=ATTENDANCE,default = 0) #assuming 1 is present 0 is absent
     cause_of_absence = models.CharField(max_length = 200,null=True,blank=True)
     _class = models.ForeignKey(Classes, on_delete= models.CASCADE)
--- oosc/oosc/attendance/views.py
+++ oosc/oosc/attendance/views.py
@@ -9,6 +9,7 @@ from oosc.attendance.serializers import AttendanceSerializer
 from oosc.absence.models import Absence
 from datetime import datetime
 # Create your views here.
+from bulk_update.helper import bulk_update
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from datetime import datetime,timedelta
@@ -170,23 +171,20 @@ class ListCreateAttendance(generics.ListAPIView):
             print daily
             return daily
 
-
-
-
-
 class TakeAttendance(APIView):
-    def post(self,request,format=None):
-        now=self.request.data["date"].replace('-','')
-        absents=[]
-        thedate=self.request.data["date"]
+    def post(self, request, format=None):
+        now = self.request.data["date"].replace('-', '')
+        absents = []
+        thedate = self.request.data["date"]
         print (request.data)
+        batchatts=[]
         try:
             for i in request.data["present"]:
                 print ("present")
-                student=Students()
-                student=Students.objects.filter(id=i)[0]
-                student.total_absents=0
-                student.last_attendance=datetime.now().date()
+                student = Students()
+                student = Students.objects.filter(id=i)[0]
+                student.total_absents = 0
+                student.last_attendance = datetime.now().date()
                 abs = Absence.objects.filter(student=student, status=False)
                 # if(len(abs)>0):
                 #     print "reason for absence needed"
@@ -194,14 +192,13 @@ class TakeAttendance(APIView):
                 #     print "added to absents"
                 # print student
                 # student.save()
-                attendance=Attendance()
-                attendance.date=thedate
-                attendance.id=now+str(i)
-                attendance.status=1
-                attendance._class=student.class_id
-                attendance.student=student
-                attendance.save()
-
+                attendance = Attendance()
+                attendance.date = thedate
+                attendance.id = now + str(i)
+                attendance.status = 1
+                attendance._class = student.class_id
+                attendance.student = student
+                batchatts.append(attendance)
                 print (attendance.id)
             for i in request.data["absent"]:
                 print ("Absemt")
@@ -232,13 +229,83 @@ class TakeAttendance(APIView):
                 attendance.status = 0
                 attendance._class = student.class_id
                 attendance.student = student
-                attendance.save()
-            print("Done replying")
-            absnts=Absence.objects.filter(student__in=absents)
-            #ser=DetailedAbsenceserializer(absnts,many=True)
-            return Response(data=[],status=status.HTTP_201_CREATED)
+                batchatts.append(attendance)
+            #tt=Attendance.objects.bulk_create(batchatts)
+            bulk_update(batchatts)
+            print("Done replying ")
+            absnts = Absence.objects.filter(student__in=absents)
+            # ser=DetailedAbsenceserializer(absnts,many=True)
+            return Response(data=[], status=status.HTTP_201_CREATED)
         except Exception as e:
-            return Response(data={"error":"Error","error_description":e.message},status=status.HTTP_400_BAD_REQUEST)
+            return Response(data={"error": "Error", "error_description": e.message},status=status.HTTP_400_BAD_REQUEST)
+
+
+#
+# class TakeAttendance(APIView):
+#     def post(self,request,format=None):
+#         now=self.request.data["date"].replace('-','')
+#         absents=[]
+#         thedate=self.request.data["date"]
+#         print (request.data)
+#         try:
+#             for i in request.data["present"]:
+#                 print ("present")
+#                 student=Students()
+#                 student=Students.objects.filter(id=i)[0]
+#                 student.total_absents=0
+#                 student.last_attendance=datetime.now().date()
+#                 abs = Absence.objects.filter(student=student, status=False)
+#                 # if(len(abs)>0):
+#                 #     print "reason for absence needed"
+#                 #     absents.append(student)
+#                 #     print "added to absents"
+#                 # print student
+#                 # student.save()
+#                 attendance=Attendance()
+#                 attendance.date=thedate
+#                 attendance.id=now+str(i)
+#                 attendance.status=1
+#                 attendance._class=student.class_id
+#                 attendance.student=student
+#                 attendance.save()
+#
+#                 print (attendance.id)
+#             for i in request.data["absent"]:
+#                 print ("Absemt")
+#                 student = Students()
+#                 student = Students.objects.filter(id=i)[0]
+#                 # student.total_absents = student.total_absents + 1
+#                 # if(student.total_absents>4):
+#                 #     abs=Absence.objects.filter(student=student,status=False)
+#                 #     if(len(abs)>0):
+#                 #         print ("found")
+#                 #         abs[0].date_to=datetime.now().date()
+#                 #         abs[0].save()
+#                 #     else:
+#                 #         print ("saving ...")
+#                 #         ab=Absence()
+#                 #         ab.student=student
+#                 #         ab.date_to=datetime.now().date()
+#                 #         ab.date_from=student.last_attendance
+#                 #         ab.status=False
+#                 #         ab.save()
+#                 #         print ("Saved ...")
+#                 # else:
+#                 #     print ("within ")
+#                 #     student.save()
+#                 attendance = Attendance()
+#                 attendance.date = thedate
+#                 attendance.id = now + str(i)
+#                 attendance.status = 0
+#                 attendance._class = student.class_id
+#                 attendance.student = student
+#                 attendance.save()
+#             print("Done replying")
+#             absnts=Absence.objects.filter(student__in=absents)
+#             #ser=DetailedAbsenceserializer(absnts,many=True)
+#             return Response(data=[],status=status.HTTP_201_CREATED)
+#         except Exception as e:
+#             return Response(data={"error":"Error","error_description":e.message},status=status.HTTP_400_BAD_REQUEST)
 
 class WeeklyAttendanceReport(APIView):
     def get(self,request,format=None):
