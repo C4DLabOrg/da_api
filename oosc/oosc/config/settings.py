7f6958149a7aff995460dfd048f7c939fb5e90b2
Micha Mwangi <michameiu@gmail.com>
2017-08-22 17:22:53 +0300

    added the subcounties to counties serializer

--- oosc/oosc/config/settings.py
+++ oosc/oosc/config/settings.py
@@ -86,6 +86,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     # 'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
--- oosc/oosc/counties/serializers.py
+++ oosc/oosc/counties/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from oosc.counties.models import Counties
 from rest_framework import serializers
+
+from subcounty.serializers import  SimlpeSubCountySerializer
+
+
 class CountiesSerializer(serializers.ModelSerializer):
+    subcounties = SimpleSubCountySerializer(many=True, read_only=True)
     class Meta:
         model = Counties
-        fields = ('county_name','id')
+        fields = ('county_name','id','subcounties')
         
