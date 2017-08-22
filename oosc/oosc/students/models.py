ac005d8208475032175bac95a8686460d2fee6a0
Micha Mwangi <michameiu@gmail.com>
2017-08-22 18:28:09 +0300

    student_id to null

--- oosc/oosc/students/models.py
+++ oosc/oosc/students/models.py
@@ -5,9 +5,9 @@ from oosc.schools.models import Schools
 # Create your models here.
 class Students(models.Model):
     GENDERS = (('M', 'MALE'), ('F', 'FEMALE'))
-    student_id    = models.BigIntegerField(default=0,null=True,blank=True)
+    student_id    = models.BigIntegerField(null=True,blank=True)
     #school_id     = models.ForeignKey(Schools,on_delete = models.CASCADE)
-    emis_code     = models.BigIntegerField(default=0,null=True,blank=True)
+    emis_code     = models.BigIntegerField(null=True,blank=True)
     fstname  = models.CharField(max_length=200)
     midname = models.CharField(max_length=200,null=True,blank=True)
     lstname = models.CharField(max_length=200,null=True,blank=True)
