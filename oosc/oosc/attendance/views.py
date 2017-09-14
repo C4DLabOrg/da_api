c73a22489c48e9e4b52cc4b441785dc55eca2031
Micha Mwangi <michameiu@gmail.com>
2017-09-15 00:59:08 +0300

    added the absent filter for absent students

--- oosc/oosc/attendance/views.py
+++ oosc/oosc/attendance/views.py
@@ -170,7 +170,7 @@ class ListCreateAttendance(generics.ListAPIView):
         at = data.annotate(month=self.get_format(format=format)).values("month")
         at=at.annotate(present_males=pm, present_females=pf,absent_males=am, absent_females=af,value=outp)
         #at=at.annotate(value=Concat(Value(queryet),Value(""),output_field=CharField()))
-
+        print (at)
         at=at.exclude(present_males=0,present_females=0,absent_males=0,absent_females=0)
         print (at)
         return at.order_by(outp)
