3ba61c8f1eff328fd1a59ba3e13c7887b48d3050
Mwangi Micha <michameiu@gmail.com>
2018-04-01 18:57:22 +0300

    added the production server ;

--- oosc/oosc/config/settings.py
+++ oosc/oosc/config/settings.py
@@ -140,16 +140,17 @@ DATABASES = {
 
 #Production Only
 os.environ['TZ'] = 'Africa/Nairobi'
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'oosc',        # Or path to database file if using sqlite3.
-#         'USER': 'sisitech',                   # Not used with sqlite3.
-#         'PASSWORD': '#sisitech',            # Not used with sqlite3.
-#         'HOST': 'localhost',             # Set to empty string for localhost. Not used with sqlite3.
-#         'PORT': '5432',                  # Set to empty string for default. Not used with sqlite3.
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'oosc',        # Or path to database file if using sqlite3.
+        'USER': 'sisitech',                   # Not used with sqlite3.
+        'PASSWORD': '#sisitech',            # Not used with sqlite3.
+        'HOST': 'localhost',             # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                  # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
 # CACHES = {
 #     'default': {
 #         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
