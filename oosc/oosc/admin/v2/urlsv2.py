b0d85eb3b704bf793aafd8bfa3733f7d5e9b2273
Mwangi Micha <michameiu@gmail.com>
2018-04-25 14:47:54 +0300

    added delete streams data

--- oosc/oosc/admin/v2/urlsv2.py
+++ oosc/oosc/admin/v2/urlsv2.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 
-from oosc.admin.v2.views import RestPassword, DeleteStreams, RetrieveDeleteStream
+from oosc.admin.v2.views import RestPassword, DeleteStreams, RetrieveDeleteStream, DeleteStudentsByStreams
 
 urlpatterns = [
     url(r'^reset-password', RestPassword.as_view(),name="reset_password"),
     url(r'^delete-schools-data', DeleteStreams.as_view(),name="reset_password"),
-    url(r'^delete-streams-data', DeleteStreams.as_view(),name="delete_streams_password"),
+    url(r'^delete-streams-data', DeleteStudentsByStreams.as_view(),name="delete_streams_password"),
     url(r'^teachers/(?P<pk>\w{3,17})/reset', RetrieveDeleteStream.as_view(),name="reset_password"),
 ]
